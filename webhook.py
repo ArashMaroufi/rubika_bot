c89c8560e892194a8134b6c1b1f6fb8a8917ad87
ARASH MAROUFI <a.maroufi@Rub.local>
2025-08-05 17:32:46 +0330

    change bot token

--- webhook.py
+++ webhook.py
@@ -1,17 +1,38 @@
+# import requests
+
+# BOT_TOKEN = "BIABH0NLKZETIEBOCAMXQAEJEPAZMVMEPONHMEHNVXGHRPLRVJQTTAQECKNOKELX"
+# WEBHOOK_URL = "https://rubika-bot-spz2.onrender.com/receiveUpdate"
+
+# # url = f'https://botapi.rubika.ir/v3/{BOT_TOKEN}/updateEndpoint'
+
+# data = {
+#     "url": WEBHOOK_URL,
+#     "type": "GetSelectionItem"
+# }
+# url = f"https://botapi.rubika.ir/v3/{BOT_TOKEN}/updateBotEndpoints"
+
+# response = requests.post(url, json=data)
+
+# print("Status Code:", response.status_code)
+# print("Response:", response.text)
 import requests
 
-BOT_TOKEN = "BIABH0NLKZETIEBOCAMXQAEJEPAZMVMEPONHMEHNVXGHRPLRVJQTTAQECKNOKELX"
-WEBHOOK_URL = "https://rubika-bot-spz2.onrender.com/receiveUpdate"
+url = "https://rubika-bot-spz2.onrender.com/receiveUpdate"
 
-# url = f'https://botapi.rubika.ir/v3/{BOT_TOKEN}/updateEndpoint'
-
-data = {
-    "url": WEBHOOK_URL,
-    "type": "GetSelectionItem"
+payload = {
+    "inline_message": {
+        "sender_id": "test-user",
+        "text": "سلام",
+        "location": None,
+        "aux_data": {
+            "start_id": None,
+            "button_id": "test-btn"
+        },
+        "message_id": "1234567890",
+        "chat_id": "chat-1234"
+    }
 }
-url = f"https://botapi.rubika.ir/v3/{BOT_TOKEN}/updateBotEndpoints"
 
-response = requests.post(url, json=data)
-
-print("Status Code:", response.status_code)
-print("Response:", response.text)
+response = requests.post(url, json=payload)
+print(response.status_code)
+print(response.text)
