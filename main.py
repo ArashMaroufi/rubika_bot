548d0f676d9518bd4487b6cd345fdd4c46e86ef7
ARASH MAROUFI <a.maroufi@Rub.local>
2025-08-06 12:19:16 +0330

    update code

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
 from flask import Flask, request, jsonify
 # import os
 import requests
+import logging
 
 app = Flask(__name__)
 
 BOT_TOKEN = "BJAIE0CCEQHJEQYDFOATWSRHEJWBSONBYQWJMXNJYRHYULEGNMFEVKXGMGMPSGDK"
 
-
-# def send_message(chat_id, text):
-#     url = f"https://botapi.rubika.ir/v3/{BOT_TOKEN}/sendMessage"
-#     payload = {
-#         "chat_id": chat_id,
-#         "text": text,
-#     }
-#     response = requests.post(url, json=payload)
-#     print("🔵 SendMessage Status:", response.status_code)
-#     print("🔵 SendMessage Response:", response.text)
-#     return response.json()
+logging.basicConfig(level=logging.INFO)
 
 
 @app.route("/", methods=["GET"])
@@ -26,33 +17,16 @@ def index():
 
 @app.route('/receiveUpdate', methods=['POST'])
 def receive_update():
-    print("🔔 درخواست POST دریافت شد")
+    logging.info("🔔 درخواست POST دریافت شد")
 
     try:
         data = request.get_json(force=True)
-        print("📩 داده دریافتی:")
-        print(data)
+        logging.info(f"📩 داده دریافتی:\n{data}")
     except Exception as e:
-        print("❌ خطا در خواندن JSON:")
-        print(e)
+        logging.error("❌ خطا در خواندن JSON:")
+        logging.error(e)
 
     return "OK", 200
-
-
-# @app.route("/receiveInlineMessage", methods=["POST"])
-# def receive_inline():
-#     data = request.get_json()
-#     print("📩 receiveInlineMessage:", data)
-
-#     update = data.get("update", {})
-#     new_msg = update.get("new_message", {})
-#     chat_id = update.get("chat_id")
-#     text = new_msg.get("text")
-
-#     if chat_id and text:
-#         send_message(chat_id, f"✅ پیام شما دریافت شد: {text}")
-
-#     return jsonify({"ok": True})
 
 
 if __name__ == "__main__":
